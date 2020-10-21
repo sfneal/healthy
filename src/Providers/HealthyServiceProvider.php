f22b6f2d853ac9f33ef3478851a904d10c328e59
Stephen Neal <stephen.neal14@gmail.com>
2020-10-21 11:28:27 -0400

    FIX route registration & MAKE LaravelTest

--- src/Providers/HealthyServiceProvider.php
+++ src/Providers/HealthyServiceProvider.php
@@ -4,6 +4,7 @@ namespace Sfneal\Healthy\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Sfneal\Healthy\Controllers\HealthyController;
 
 class HealthyServiceProvider extends ServiceProvider
 {
@@ -16,11 +17,11 @@ class HealthyServiceProvider extends ServiceProvider
     {
         // Publish config file
         $this->publishes([
-            __DIR__.'/../config/healthy.php' => base_path('config/healthy.php'),
+            __DIR__.'/../../config/healthy.php' => base_path('config/healthy.php'),
         ], 'config');
 
         // Map health check paths
-        Route::middleware('web')->group(base_path('routes/health.php'));
+        Route::get(config('healthy.route'), HealthyController::class)->name('health');
     }
 
     /**
@@ -31,6 +32,6 @@ class HealthyServiceProvider extends ServiceProvider
     public function register()
     {
         // Load config file
-        $this->mergeConfigFrom(__DIR__.'/../config/healthy.php', 'healthy');
+        $this->mergeConfigFrom(__DIR__.'/../../config/healthy.php', 'healthy');
     }
 }
---
+++ tests/LaravelTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Sfneal\Healthy\Tests;
+
+use Illuminate\Foundation\Application;
+use Orchestra\Testbench\TestCase;
+use Sfneal\Healthy\Providers\HealthyServiceProvider;
+
+class LaravelTest extends TestCase
+{
+    /**
+     * Register package service providers
+     *
+     * @param Application $app
+     * @return array|string
+     */
+    protected function getPackageProviders($app)
+    {
+        return HealthyServiceProvider::class;
+    }
+
+    /** @test */
+    public function the_route_can_be_accessed()
+    {
+        // Get request the route
+        $this->get(config('healthy.route'))
+            ->assertSeeText('OK')
+            ->assertStatus(200);
+    }
+}
